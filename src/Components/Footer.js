61b7f68369a809e45e05100cc57862a427b051a5
Zeeeforzain <zeeforzain@gmail.com>
2024-09-12 18:26:43 +0500

    Footer Done

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,12 +3,39 @@ import BoxComponent from './Box';
 import TypographyComponent from './Typography';
 export default function Footer() {
   return (
-    <BoxComponent>
-      <BoxComponent>
-        <TypographyComponent>رابطہ کریں</TypographyComponent>
-        <TypographyComponent>اشتہارات</TypographyComponent>
-        <TypographyComponent>پالیسی</TypographyComponent>
+    <BoxComponent
+    backgroundColor='var(--dull)'
+    display='flex'
+    alignItems='center'
+    justifyContent='space-evenly'
+    flexDirection='column'
+    color='var(--light)'
+    height='12vh'
+    >
+      <BoxComponent
+      sx={{
+        display:'flex',
+        width:'20%',
+        justifyContent:'space-between',
+        alignItems:'center',
+      }}
+      >
+        <TypographyComponent
+        width='100px'
+        borderRight='1px solid var(--light)'
+        sx={{cursor:'pointer'}}
+        >پرائیویسی پالیسی</TypographyComponent>
+        <TypographyComponent
+          width='60px'
+          sx={{cursor:'pointer'}}
+        borderRight='1px solid var(--light)'
+        >اشتہارات</TypographyComponent>
+        <TypographyComponent
+          width='80px'
+          sx={{cursor:'pointer'}}
+        >رابطہ کریں</TypographyComponent>
       </BoxComponent>
+      <TypographyComponent>Copyright © 2024. All Rights Reserved</TypographyComponent>
     </BoxComponent>
   );
 }
