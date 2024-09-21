0aa335f85eef8ec6ac7207fea1ef94abd2387ccd
Zeeeforzain <zeeforzain@gmail.com>
2024-09-21 15:29:24 +0500

    page fixed

--- src/ExtendedPage.jsx
+++ src/ExtendedPage.jsx
@@ -14,44 +14,46 @@ export default function ExtendedPage() {
 
   return (
     <>
-    <Head/>
-    <BoxComponent
-      width="auto"
-      display={{ xs: "none", sm: "flex" }}
-      justifyContent="right"
-      paddingRight={{ xs: "10px", sm: "10px", md: "10px", lg: "100px" }}
-    >
-      <Title
-        sx={{
-          fontFamily: "var(--main)",
-          fontSize: { sm: "35px", md: "40px", lg: "50px" },
-          fontWeight: "700",
-          color: "var(--primary)",
-        }}
-      />
-    </BoxComponent>
-    <Navbar />
-    <BoxComponent>
+      <Head />
       <BoxComponent
-        display="flex"
-        flexDirection={{xs:'column-reverse', md:'row'}}
-        justifyContent='space-evenly'
+        width="auto"
+        display={{ xs: "none", sm: "flex" }}
+        justifyContent="right"
+        paddingRight={{ xs: "10px", sm: "10px", md: "10px", lg: "100px" }}
       >
-        <BoxComponent width={{xs:'95%',md:"30%"}} margin='auto'>
-          <ColumnBox />
+        <Title
+          sx={{
+            fontFamily: "var(--main)",
+            fontSize: { sm: "35px", md: "40px", lg: "50px" },
+            fontWeight: "700",
+            color: "var(--primary)",
+          }}
+        />
+      </BoxComponent>
+      <Navbar />
+      <BoxComponent>
+        <BoxComponent
+          display="flex"
+          flexDirection={{ xs: "column-reverse", md: "row" }}
+          justifyContent="space-around" // Ensuring alignment starts from the left
+          alignItems="flex-start" // Align vertically to the start for medium screens and above
+        >
+          <BoxComponent width={{ xs: "95%", md: "30%" }} margin={{ xs: "auto", md: "0" }}>
+            <ColumnBox />
+          </BoxComponent>
+          <BoxComponent width={{ xs: "95%", md: "60%" }} margin={{ xs: "auto", md: "0" }}>
+            <ExtendedNews title={title} img={img} detail={detail} />
+          </BoxComponent>
         </BoxComponent>
-        <BoxComponent width={{xs:'95%', md:"60%"}} margin='auto'>
-          <ExtendedNews title={title} img={img} detail={detail} /> {/* Pass props to ExtendedNews */}
+        <BoxComponent
+          display="flex"
+          justifyContent={{ xs: "center", md: "right" }}
+          padding={{ xs: "0px", md: "50px" }}
+        >
+          <MostReadNews width={{ xs: "95%", md: "30%" }} />
         </BoxComponent>
       </BoxComponent>
-      <BoxComponent
-        display='flex'
-        justifyContent={{xs:'center', md:'right'}}
-        padding={{xs:'0px',md:'50px'}}
-      >
-        <MostReadNews width={{xs:'95%',md:'30%'}} />
-      </BoxComponent>
-    </BoxComponent>
     </>
   );
 }
+
